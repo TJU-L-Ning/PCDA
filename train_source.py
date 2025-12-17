b8bdde12fdcb2f0474e0348f712c6bc5b392f312
L_ning <146630884+TJU-L-Ning@users.noreply.github.com>
2025-12-17 14:50:01 +0800

    Update train_source.py

--- train_source.py
+++ train_source.py
@@ -42,15 +42,14 @@ def train(args, model, dataloader, criterion, optimizer, epoch_idx=0.0):
     for imgs_train, _, imgs_label, _ in tqdm(dataloader, ncols=60):
         
         iter_idx += 1
-        #pdb.set_trace() ###
+
 
         imgs_train = imgs_train.cuda()
         imgs_label = imgs_label.cuda()
         
-        _, pred_cls = model(imgs_train, apply_softmax=True) ##获取伪标签
-        imgs_onehot_label = torch.zeros_like(pred_cls).scatter(1, imgs_label.unsqueeze(1), 1) ##伪标签的精细化处理
-        
-        loss = criterion(pred_cls, imgs_onehot_label)   ###损失函数-平滑交叉损失熵
+        _, pred_cls = model(imgs_train, apply_softmax=True)
+        imgs_onehot_label = torch.zeros_like(pred_cls).scatter(1, imgs_label.unsqueeze(1), 1) 
+        loss = criterion(pred_cls, imgs_onehot_label)   
         
         lr_scheduler(optimizer, iter_idx, iter_max)
         optimizer.zero_grad()
@@ -64,13 +63,13 @@ def train(args, model, dataloader, criterion, optimizer, epoch_idx=0.0):
     return train_loss
 
 @torch.no_grad()
-def test(args, model, dataloader, src_flg=True):  ##src_flg决定是否用h-score来评估网络性能
+def test(args, model, dataloader, src_flg=True):
     model.eval()
     gt_label_stack = []
     pred_cls_stack = []
     
     if src_flg:
-        class_list = args.source_class_list  ##类别的list
+        class_list = args.source_class_list 
         open_flg = False
     else:
         class_list = args.target_class_list
@@ -87,34 +86,31 @@ def test(args, model, dataloader, src_flg=True):  ##src_flg决定是否用h-scor
         gt_label_stack.append(imgs_label)
         pred_cls_stack.append(pred_cls.cpu())
         
-    gt_label_all = torch.cat(gt_label_stack, dim=0) #[N]
-    #pdb.set_trace()
-    pred_cls_all = torch.cat(pred_cls_stack, dim=0) #[N, C]
+    gt_label_all = torch.cat(gt_label_stack, dim=0) 
+
+    pred_cls_all = torch.cat(pred_cls_stack, dim=0) 
     
     h_score, known_acc,\
     unknown_acc, per_cls_acc = get_acc(args, class_list, gt_label_all, pred_cls_all, open_flg)
-    ###
+
     return h_score, known_acc, unknown_acc, per_cls_acc
     
 def main(args):
     
     os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
-    this_dir = os.path.join(os.path.dirname(__file__), ".")  #当前脚本文件所在的目录的路径为/home/ubuntu/HDDs/HDD1/lyt/GLC-main/.
+    this_dir = os.path.join(os.path.dirname(__file__), ".") 
     
     model = SFUniDA(args) 
-    if args.checkpoint is not None and os.path.isfile(args.checkpoint): ##给定的 args.checkpoint 是否为一个有效的文件路径，并且文件存在
+    if args.checkpoint is not None and os.path.isfile(args.checkpoint):
         save_dir = os.path.dirname(args.checkpoint)
         checkpoint = torch.load(args.checkpoint, map_location=torch.device("cpu"))
-        model.load_state_dict(checkpoint["model_state_dict"])   ##是否加载已经训练好的模型
+        model.load_state_dict(checkpoint["model_state_dict"])   
     else:
-        save_dir = os.path.join(this_dir, args.save_checkpoints, args.dataset, "source_{}".format(args.s_idx),  ##修改checkpoint路径------------------------------
+        save_dir = os.path.join(this_dir, args.save_checkpoints, args.dataset, "source_{}".format(args.s_idx), 
                                 "source_{}_{}".format(args.source_train_type, args.target_label_type))
-        ###是否存在这样一个目录
 
         if not os.path.isdir(save_dir):
             os.makedirs(save_dir)
-
-        ###不存在这个目录就新建
             
     model.cuda()
     args.save_dir = save_dir     
@@ -131,14 +127,13 @@ def main(args):
     optimizer = torch.optim.SGD(params_group)
     optimizer = op_copy(optimizer)
     
-##-------------------------------------------------------------------------------------------------------------------
     source_data_list = open(os.path.join(args.source_data_dir, args.train_source_list), "r").readlines()  
     source_dataset = SFUniDADataset(args, args.source_data_dir, source_data_list, d_type="source", preload_flg=True)
     source_dataloader = DataLoader(source_dataset, batch_size=args.batch_size, shuffle=True,
                                    num_workers=args.num_workers, drop_last=True)
     
-    target_dataloader_list = []   ##
-    target_dataloader_list1 = []   ##
+    target_dataloader_list = []  
+    target_dataloader_list1 = [] 
     for idx in range(len(args.target_domain_dir_list)):
         target_data_dir = args.target_domain_dir_list[idx]
         target_data_list = open(os.path.join(target_data_dir, args.train_source_list), "r").readlines() 
@@ -166,18 +161,18 @@ def main(args):
     
     for epoch_idx in tqdm(range(args.epochs), ncols=60):
         
-        train_loss = train(args, model, source_dataloader, criterion, optimizer, epoch_idx) ##计算总的损失，目标域数据集的测试得分不笼入，只使用源域的数据来优化
+        train_loss = train(args, model, source_dataloader, criterion, optimizer, epoch_idx) 
         logger.info("Epoch:{}/{} train_loss:{:.3f}".format(epoch_idx, args.epochs, train_loss))
         
         if epoch_idx % 1 == 0:
-            # EVALUATE ON SOURCE
-            _, source_known_acc, _, src_per_cls_acc = test(args, model, source_dataloader, src_flg=True)##源数据里面也分了公共类和私有类
+
+            _, source_known_acc, _, src_per_cls_acc = test(args, model, source_dataloader, src_flg=True)
             logger.info("EVALUATE ON SOURCE: KnownAcc:{:.3f}".format(source_known_acc))
-            ##关键信息写入日志
+
             
             if args.dataset == "VisDA":
                 logger.info("VISDA PER_CLS_ACC:")
-                logger.info(src_per_cls_acc) ##mi3dor暂时不需要类别的分类准确率
+                logger.info(src_per_cls_acc)
            
             if args.dataset == "MI3DOR":
                 logger.info("MI3DOR PER_CLS_ACC:")
@@ -187,11 +182,7 @@ def main(args):
         checkpoint_file = "latest_source_checkpoint.pth"
         torch.save({
             "epoch":epoch_idx,
-            "model_state_dict":model.state_dict()}, os.path.join(save_dir, checkpoint_file))   ##没有比较，储存的就是最终的模型
-        ##模型存储路径
-
-
-        
+            "model_state_dict":model.state_dict()}, os.path.join(save_dir, checkpoint_file))
 
     for idx_i, item in enumerate(args.source_domain_list):
         notation_str =  "\n=================================================\n"
